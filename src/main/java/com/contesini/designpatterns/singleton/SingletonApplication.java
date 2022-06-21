120953f345f755d0d1ddef300708482d42e01943
Lucas Contesini <lucas.contesini@hotmail.com>
2022-06-21 19:37:22 -0300

    Add Singleton

---
+++ src/main/java/com/contesini/designpatterns/singleton/SingletonApplication.java
@@ -0,0 +1,19 @@
+package com.contesini.designpatterns.singleton;
+
+public class SingletonApplication {
+
+  public static void main(String[] args) {
+    callSingleton();
+    callSingleton();
+  }
+
+  private static void callSingleton() {
+    for (int i = 0; i < 10; i++) {
+      var singleton = Singleton.getInstance();
+
+      System.out.println(singleton);
+    }
+  }
+
+
+}
